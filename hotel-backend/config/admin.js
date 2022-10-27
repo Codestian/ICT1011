1e5dae3d0152256f823ffeba0fc26c5dc0102e89
Alan2201038 <2201038@sit.singaporetech.edu.sg>
2022-10-27 14:28:48 +0800

    feat: updated hotel-backend

--- hotel-backend/config/admin.js
+++ hotel-backend/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: 'fhfhfrhftfh',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: 'fgdfgdgg',
   },
 });
