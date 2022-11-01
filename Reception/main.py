2c6c100dd685bbe9b64586b9304077b3a5eedbb8
Alan2201038 <2201038@sit.singaporetech.edu.sg>
2022-11-01 14:24:21 +0800

    feat: Updated Reception.

--- Reception/main.py
+++ Reception/main.py
@@ -1,8 +1,10 @@
 from unittest import result
 from flask import Flask,render_template,request
 import urllib.request, json
+from keys import func1
 app = Flask(__name__)
- 
+
+
 @app.route("/")
 def hello_world():
     url = "http://localhost:1337/api/rooms?populate=*"
@@ -13,14 +15,16 @@ def hello_world():
     hotelInformation = {}
     
     for hotel in dict["data"]:
-        for key,value in hotel['attributes'].items():
-            if "number" in key:
-                hotelRoom.append(value)
-            if "attributes" in key:
-                hotelType.append(value="text")
-            else:
-                hotelType.append("dead")
+        for key,value in hotel['availability'].items():
+            if "status" in key:
+                if "vacant" in value.lower():
+                    if "number" in hotel:
+                        hotelRoom.append((hotel["number"]))
+                    for key,value in hotel["type"].items():
+                        if "name" in key:
+                            hotelType.append(value)  
     hotelInformation  = {hotelRoom[i]: hotelType[i] for i in range(len(hotelRoom))}
+    print(hotelInformation)
     return render_template("index.html",data = hotelInformation)
 
  
@@ -30,35 +34,8 @@ def data():
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         form_data = request.form
+        print(form_data)
         return render_template('data.html',form_data = form_data)
-
-@app.route("/test/")
-def test():
-    url = "http://localhost:1337/api/rooms?populate=*"
-    response = urllib.request.urlopen(url)
-    hotels = response.read()
-    dict = json.loads(hotels)
-    return render_template('test.html', title="page", jsonfile=json.dumps(dict))
-
-@app.route("/test2/")
-def test2():
-    url = "http://localhost:1337/api/rooms?populate=*"
-    response = urllib.request.urlopen(url)
-    hotels = response.read()
-    dict = json.loads(hotels)
-    hotelRoom,hotelType = [], []
-    hotelInformation = {}
-    
-    for hotel in dict["data"]:
-        for key,value in hotel['attributes'].items():
-            if "number" in key:
-                hotelRoom.append(value)
-            if "attributes" in key:
-                hotelType.append(value="text")
-            else:
-                hotelType.append("dead")
-    hotelInformation  = {hotelRoom[i]: hotelType[i] for i in range(len(hotelRoom))}
-    return render_template("dicttest.html",result = hotelInformation)
  
 if __name__ == "__main__":
     app.run()
