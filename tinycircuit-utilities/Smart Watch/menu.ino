69b7015dd60516f02561146ddc84973b1bf6c3be
Yi Heng <113161032+UncleHengz@users.noreply.github.com>
2022-11-13 16:20:47 +0800

    feat: Tidied Code for Watch

---
+++ tinycircuit-utilities/Smart Watch/menu.ino
@@ -0,0 +1,314 @@
+
+
+typedef struct
+{
+  const uint8_t amtLines;
+  const char* const * strings;
+  void (*selectionHandler)(uint8_t);
+} menu_info;
+
+
+uint8_t menuHistory[5];
+uint8_t menuHistoryIndex = 0;
+uint8_t currentMenu = 0;
+uint8_t currentMenuLine = 0;
+uint8_t lastMenuLine = -1;
+uint8_t currentSelectionLine = 0;
+uint8_t lastSelectionLine = -1;
+
+
+void newMenu(int8_t newIndex) {
+  currentMenuLine = 0;
+  lastMenuLine = -1;
+  currentSelectionLine = 0;
+  lastSelectionLine = -1;
+  if (newIndex >= 0) {
+    menuHistory[menuHistoryIndex++] = currentMenu;
+    currentMenu = newIndex;
+  } else {
+    if (currentDisplayState == displayStateMenu) {
+      menuHistoryIndex--;
+      currentMenu = menuHistory[menuHistoryIndex];
+    }
+  }
+  if (menuHistoryIndex) {
+    currentDisplayState = displayStateMenu;
+    if (menu_debug_print)SerialMonitorInterface.print("New menu index ");
+    if (menu_debug_print)SerialMonitorInterface.println(currentMenu);
+  } else {
+    if (menu_debug_print)SerialMonitorInterface.print("New menu index ");
+    if (menu_debug_print)SerialMonitorInterface.println("home");
+    currentDisplayState = displayStateHome;
+    initHomeScreen();
+  }
+}
+
+static const char PROGMEM mainMenuStrings0[] = "Set auto off";
+static const char PROGMEM mainMenuStrings1[] = "Set brightness";
+static const char PROGMEM mainMenuStrings2[] = "Timer";
+static const char PROGMEM mainMenuStrings3[] = "Stopwatch";
+
+static const char* const PROGMEM mainMenuStrings[] =
+{
+  mainMenuStrings0,
+  mainMenuStrings1,
+  mainMenuStrings2,
+  mainMenuStrings3,
+};
+
+const menu_info mainMenuInfo =
+{
+  4,
+  mainMenuStrings,
+  mainMenu,
+};
+
+const menu_info menuList[] = {mainMenuInfo};
+#define mainMenuIndex 0
+
+int currentVal = 0;
+int digits[4];
+int currentDigit = 0;
+int maxDigit = 4;
+int *originalVal;
+void (*editIntCallBack)() = NULL;
+
+uint8_t timeMenu(uint8_t button)
+{
+    display.clearWindow(0, 10, 96, 64);
+    display.setFont(font10pt);
+    display.fontColor(defaultFontColor, defaultFontBG);
+    display.setCursor(0, menuTextY[0]);
+    display.print(F("< back"));
+
+    if (button == backButton){
+    viewMenu(backButton);
+    return 1;
+    }
+}
+
+uint8_t editTimer(uint8_t button, int *inVal, char *intName, void (*cb)()) {
+  if (menu_debug_print)SerialMonitorInterface.println("editInt");
+  if (!button) {
+    if (menu_debug_print)SerialMonitorInterface.println("editIntInit");
+    editIntCallBack = cb;
+    currentDisplayState = displayStateEditor;
+    editorHandler = editInt;
+    currentDigit = 0;
+    originalVal = inVal;
+    currentVal = *originalVal;
+    digits[3] = currentVal % 10; currentVal /= 10;
+    digits[2] = currentVal % 10; currentVal /= 10;
+    digits[1] = currentVal % 10; currentVal /= 10;
+    digits[0] = currentVal % 10;
+    currentVal = *originalVal;
+    display.clearWindow(0, 10, 96, 64);
+    display.setFont(font10pt);
+    display.fontColor(defaultFontColor, defaultFontBG);
+    display.setCursor(0, menuTextY[0]);
+    display.print(F("< back/undo"));
+    display.setCursor(90, menuTextY[0]);
+    display.print('^');
+    display.setCursor(0, menuTextY[3]);
+    display.print(F("< next/save"));
+    display.setCursor(90, menuTextY[3]);
+    display.print('v');
+  } else if (button == upButton) {
+    if (digits[currentDigit] < 9)
+      digits[currentDigit]++;
+  } else if (button == downButton) {
+    if (digits[currentDigit] > 0)
+      digits[currentDigit]--;
+  } else if (button == selectButton) {
+    if (currentDigit < maxDigit - 1) {
+      currentDigit++;
+    } else {
+      //save
+      int newValue = (digits[3]) + (digits[2] * 10) + (digits[1] * 100) + (digits[0] * 1000);
+      *originalVal = newValue;
+      viewMenu(backButton);
+      if (editIntCallBack) {
+        editIntCallBack();
+        editIntCallBack = NULL;
+      }
+      return 1;
+    }
+  } else if (button == backButton) {
+    if (currentDigit > 0) {
+      currentDigit--;
+    } else {
+      if (menu_debug_print)SerialMonitorInterface.println(F("back"));
+      viewMenu(backButton);
+      return 0;
+    }
+  }
+  display.setCursor(10, menuTextY[1]);
+  //display.setFont(font22pt);
+  for (uint8_t i = 0; i < 4; i++) {
+    if (i != currentDigit)display.fontColor(inactiveFontColor, defaultFontBG);
+    display.print(digits[i]);
+    if (i != currentDigit)display.fontColor(defaultFontColor, defaultFontBG);
+  }
+  display.print(F("   "));
+  return 0;
+}
+
+uint8_t editInt(uint8_t button, int *inVal, char *intName, void (*cb)()) {
+  if (menu_debug_print)SerialMonitorInterface.println("editInt");
+  if (!button) {
+    if (menu_debug_print)SerialMonitorInterface.println("editIntInit");
+    editIntCallBack = cb;
+    currentDisplayState = displayStateEditor;
+    editorHandler = editInt;
+    currentDigit = 0;
+    originalVal = inVal;
+    currentVal = *originalVal;
+    digits[3] = currentVal % 10; currentVal /= 10;
+    digits[2] = currentVal % 10; currentVal /= 10;
+    digits[1] = currentVal % 10; currentVal /= 10;
+    digits[0] = currentVal % 10;
+    currentVal = *originalVal;
+    display.clearWindow(0, 10, 96, 64);
+    display.setFont(font10pt);
+    display.fontColor(defaultFontColor, defaultFontBG);
+    display.setCursor(0, menuTextY[0]);
+    display.print(F("< back/undo"));
+    display.setCursor(90, menuTextY[0]);
+    display.print('^');
+    display.setCursor(10, menuTextY[1]);
+    display.print(intName);
+    display.setCursor(0, menuTextY[3]);
+    display.print(F("< next/save"));
+    display.setCursor(90, menuTextY[3]);
+    display.print('v');
+  } else if (button == upButton) {
+    if (digits[currentDigit] < 9)
+      digits[currentDigit]++;
+  } else if (button == downButton) {
+    if (digits[currentDigit] > 0)
+      digits[currentDigit]--;
+  } else if (button == selectButton) {
+    if (currentDigit < maxDigit - 1) {
+      currentDigit++;
+    } else {
+      //save
+      int newValue = (digits[3]) + (digits[2] * 10) + (digits[1] * 100) + (digits[0] * 1000);
+      *originalVal = newValue;
+      viewMenu(backButton);
+      if (editIntCallBack) {
+        editIntCallBack();
+        editIntCallBack = NULL;
+      }
+      return 1;
+    }
+  } else if (button == backButton) {
+    if (currentDigit > 0) {
+      currentDigit--;
+    } else {
+      if (menu_debug_print)SerialMonitorInterface.println(F("back"));
+      viewMenu(backButton);
+      return 0;
+    }
+  }
+  display.setCursor(10, menuTextY[2]);
+  for (uint8_t i = 0; i < 4; i++) {
+    if (i != currentDigit)display.fontColor(inactiveFontColor, defaultFontBG);
+    display.print(digits[i]);
+    if (i != currentDigit)display.fontColor(defaultFontColor, defaultFontBG);
+  }
+  display.print(F("   "));
+  return 0;
+}
+
+void mainMenu(uint8_t selection) {
+  if (menu_debug_print)SerialMonitorInterface.println("mainMenuHandler");
+  /*if (selection == 0) {
+    newMenu(dateTimeMenuIndex);
+  }*/
+  if (selection == 0) {
+    char buffer[20];
+    strcpy_P(buffer, (PGM_P)pgm_read_word(&(menuList[mainMenuIndex].strings[selection])));
+    editInt(0, &sleepTimeout, buffer, NULL);
+  }
+  if (selection == 1) {
+    char buffer[20];
+    strcpy_P(buffer, (PGM_P)pgm_read_word(&(menuList[mainMenuIndex].strings[selection])));
+    editInt(0, &brightness, buffer, NULL);
+  }
+  if (selection == 2) {
+    char buffer[20];
+    //strcpy_P(buffer, (PGM_P)pgm_read_word(&(menuList[mainMenuIndex].strings[selection])));
+    editTimer(0, &timerMM, buffer, NULL);
+  }
+  if (selection == 3) {
+    //char buffer[20];
+    timeMenu(0);
+    sWatchFunction();
+  }
+}
+
+void viewMenu(uint8_t button) {
+  if (menu_debug_print)SerialMonitorInterface.print("viewMenu ");
+  if (menu_debug_print)SerialMonitorInterface.println(button);
+  if (!button) {
+    newMenu(mainMenuIndex);
+    display.clearWindow(0, 10, 96, 64);
+  } else {
+    if (button == upButton) {
+      if (currentSelectionLine > 0) {
+        currentSelectionLine--;
+      } else if (currentMenuLine > 0) {
+        currentMenuLine--;
+      }
+    } else if (button == downButton) {
+      if (currentSelectionLine < menuList[currentMenu].amtLines - 1 && currentSelectionLine < 3) {
+        currentSelectionLine++;
+      } else if (currentSelectionLine + currentMenuLine < menuList[currentMenu].amtLines - 1) {
+        currentMenuLine++;
+      }
+    } else if (button == selectButton) {
+      if (menu_debug_print)SerialMonitorInterface.print("select ");
+      if (menu_debug_print)SerialMonitorInterface.println(currentMenuLine + currentSelectionLine);
+      menuList[currentMenu].selectionHandler(currentMenuLine + currentSelectionLine);
+    } else if (button == backButton) {
+      newMenu(-1);
+      if (!menuHistoryIndex)
+        return;
+    }
+  }
+  display.setFont(font10pt);
+  if (lastMenuLine != currentMenuLine || lastSelectionLine != currentSelectionLine) {
+    if (menu_debug_print)SerialMonitorInterface.println("drawing menu ");
+    if (menu_debug_print)SerialMonitorInterface.println(currentMenu);
+    for (int i = 0; i < 4; i++) {
+      display.setCursor(7, menuTextY[i]);
+      if (i == currentSelectionLine) {
+        display.fontColor(defaultFontColor, inactiveFontBG);
+      } else {
+        display.fontColor(inactiveFontColor, inactiveFontBG);
+      }
+      if (currentMenuLine + i < menuList[currentMenu].amtLines) {
+        char buffer[20];
+        strcpy_P(buffer, (PGM_P)pgm_read_word(&(menuList[currentMenu].strings[currentMenuLine + i])));
+        display.print(buffer);
+      }
+      for (uint8_t i = 0; i < 25; i++)display.write(' ');
+      if (i == 0) {
+        display.fontColor(defaultFontColor, inactiveFontBG);
+        display.setCursor(0, menuTextY[0]);
+        display.print('<');
+        display.setCursor(90, menuTextY[0]);
+        display.print('^');
+      }
+      if (i == 3) {
+        display.fontColor(defaultFontColor, inactiveFontBG);
+        display.setCursor(0, menuTextY[3]);
+        display.print('>');
+        display.setCursor(90, menuTextY[3]);
+        display.print('v');
+      }
+    }
+    lastMenuLine = currentMenuLine;
+    lastSelectionLine = currentSelectionLine;
+  }
+}
